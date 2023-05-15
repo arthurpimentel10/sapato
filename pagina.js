4baf7752dd525962611b58d067bcd95efb1f6d11
ARTHUR LUCCIAN PIMENTEL <arthur_pimentel@estudante.sesisenai.org.br>
2023-05-15 09:41:08 -0300

    second commit

---
+++ pagina.js
@@ -0,0 +1,8 @@
+let btn_btn = document.querySelector('.btn_btn')
+console.log(btn_btn)
+let navbar_ativo = document.querySelector('.navbar')
+console.log(navbar_ativo)
+
+btn_btn.addEventListener('click',()=>{
+    navbar_ativo.classList.toggle('navbar_ativo')
+})  
